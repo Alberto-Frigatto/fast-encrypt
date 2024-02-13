827fac7105efa0dfb183a86e7f0caa751d684ad0
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-02-13 19:52:10 -0300

    chore: Adicionar informações do projeto ao __init__.py do pacote easyencrypt

--- src/easyencrypt/__init__.py
+++ src/easyencrypt/__init__.py
@@ -6,3 +6,14 @@ from ._pipeline import Pipeline
 from ._rsa import RSA
 from ._substitution import Substitution
 from ._vigenere import Vigenere
+
+__author__ = 'Alberto Frigatto de Andrade Ferreira'
+__contact__ = 'albertofrigatto.comercial@gmail.com'
+__copyright__ = 'Copyright 2024, Alberto Frigatto'
+__date__ = '2024/02/13'
+__deprecated__ = False
+__email__ = 'albertofrigatto.comercial@gmail.com'
+__license__ = 'MIT'
+__maintainer__ = 'Alberto Frigatto'
+__status__ = 'Production'
+__version__ = '0.0.1'
