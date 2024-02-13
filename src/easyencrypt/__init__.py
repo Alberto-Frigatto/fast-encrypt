bde0cb8b2b6707ecf0880e313e0b663ce3f45b4d
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-02-13 19:51:12 -0300

    chore: Adicionar imports ao __init__.py do pacote easyencrypt
    Adiciona imports às classes Pipeline e RSA.

--- src/easyencrypt/__init__.py
+++ src/easyencrypt/__init__.py
@@ -2,5 +2,7 @@ from ._atbash import Atbash
 from ._caesars_cipher import CaesarsCipher
 from ._homophonic_substitution import HomophonicSubstitution
 from ._morse_code import MorseCode
+from ._pipeline import Pipeline
+from ._rsa import RSA
 from ._substitution import Substitution
 from ._vigenere import Vigenere
